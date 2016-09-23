760640d13b8f9303ba9bef6f269fc5251a6ff8ce
Dharmadev <dharmadev.gupta@autoninja.in>
2016-09-23 13:08:53 +0530

    sidebar complete

---
+++ frontend/src/app/components/sideBar/sidebar.directive.js
@@ -0,0 +1,35 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('frontend')
+    .directive('acmeSidebar', acmeSidebar);
+
+  /** @ngInject */
+  function acmeSidebar() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'app/components/sidebar/sidebar.html',
+      // scope: {
+      //     creationDate: '='
+      // },
+      controller: sidebarController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    return directive;
+
+    /** @ngInject */
+    function sidebarController(moment,$location) {
+      var vm = this;
+
+      // "vm.creationDate" is available by directive option "bindToController: true"
+      // vm.relativeDate = moment(vm.creationDate).fromNow();
+      // vm.isActive = function(viewLocation){
+      //   return viewLocation == $location.path()
+      // }
+    }
+  }
+
+})();
